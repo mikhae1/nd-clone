e7296b94358158294477f8d75120d330ed906275
mmekhanov <mikhail.mekhanov@eurochem.ru>
2018-07-26 15:38:42 +0300

    Add initial support for db_task

---
+++ db_task/db_task.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Run mysql script at database
+
+set -eo pipefail
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${CWD}/../lib/lib.sh"
+
+load_config "$@"
+
+db_login="-h${DB_HOST} -u${DB_USER} ${DB_NAME}"
+
+log::info Starting database task...
+
+if [ ! -z "$DB_TASK" ]; then
+  log::info mysql $db_login -e \"${DB_TASK}\"
+  mysql ${db_login} "-p${DB_PWD}" -v -e "${DB_TASK}"
+fi
